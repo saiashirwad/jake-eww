303aaf3085a9581e0fb90805244a36cec0c6bef8
Sai Ashirwad <saiashirwadr@gmail.com>
2025-01-09 15:55:42 +0530

    wip

--- hkt.ts
+++ hkt.ts
@@ -3,10 +3,6 @@
 
 export type Fn = (...x: never[]) => unknown
 
-export type FnInput<F extends Kind> = F extends { f: (x: infer X) => any }
-	? X
-	: unknown
-
 export declare const _: unique symbol
 export type _ = typeof _
 
@@ -15,20 +11,24 @@ export declare abstract class Kind<F extends Fn = Fn> {
 	f: F
 }
 
+export type Input<F extends Kind> = F extends { f: (x: infer X) => any }
+	? X
+	: unknown
+
 export type pipe<T extends Kind[], X> = T extends [
 	infer Head extends Kind,
 	...infer Tail extends Kind[],
 ]
 	? [X] extends [never]
 		? never
-		: pipe<Tail, apply<Head, cast<X, FnInput<Head>>>>
+		: pipe<Tail, apply<Head, cast<X, Input<Head>>>>
 	: X
 
 export type cast<T, U> = T extends U ? T : U
 
 export type first<T extends unknown[]> = T extends [] ? never : T[0]
 
-export type apply<F extends Kind, X extends FnInput<F>> = ReturnType<
+export type apply<F extends Kind, X extends Input<F>> = ReturnType<
 	(F & {
 		readonly [_]: X
 	})["f"]
