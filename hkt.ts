01bdcc487bbc816c88ba56405d7a0c7f44c42eb6
Sai Ashirwad <saiashirwadr@gmail.com>
2025-01-09 16:07:21 +0530

    wip

--- hkt.ts
+++ hkt.ts
@@ -25,8 +25,6 @@ export type pipe<T extends Kind[], X> = T extends [
 
 export type cast<T, U> = T extends U ? T : U
 
-export type first<T extends unknown[]> = T extends [] ? never : T[0]
-
 export type apply<F extends Kind, X extends Input<F>> = ReturnType<
 	(F & {
 		readonly [_]: X
